3563aa7ecc58746630a29617bf0fa6de68e1de25
usajusaj <usajusaj@gmail.com>
2018-01-24 17:12:32 -0500

    Better loading of images and smarter layout
    Signed-off-by: usajusaj <usajusaj@gmail.com>

--- js/main.js
+++ js/main.js
@@ -7,8 +7,9 @@ function handleImage(f, ele) {
     reader.onload = (function (theFile) {
         return function (e) {
             // Render thumbnail.
-            ele.innerHTML = ['<img class="img-fluid fade" src="', e.target.result,
-                '" title="', encodeURI(theFile.name), '" onload="this.classList.add(\'show\')"/>'].join('');
+            ele.innerHTML = ['<img class="img-fluid" src="', e.target.result,
+                '" title="', encodeURI(theFile.name),
+                '" onload="this.parentElement.classList.add(\'show\'); updateNewItem();"/>'].join('');
         };
     })(f);
 
@@ -21,10 +22,10 @@ function handleDat(f, ele) {
     var sizes, div;
 
     reader.onloadend = function (evt) {
-        if (evt.target.readyState == FileReader.DONE) { // DONE == 2
-            if (evt.target.result.slice(0, 24) == 'Colony Project Data File') {
+        if (evt.target.readyState === FileReader.DONE) { // DONE == 2
+            if (evt.target.result.slice(0, 24) === 'Colony Project Data File') {
                 sizes = readColony(evt.target.result);
-            } else if (evt.target.result.slice(0, 8) == '# gitter') {
+            } else if (evt.target.result.slice(0, 8) === '# gitter') {
                 sizes = readGitter(evt.target.result);
             } else {
                 console.log('Unknown data file format');
@@ -73,26 +74,59 @@ function handleDat(f, ele) {
                     b: 0,
                     t: 0,
                     pad: 0
-                },
+                }
             };
 
             Plotly.newPlot(div, data, layout, {displayModeBar: false});
+            ele.classList.add('show');
+            updateNewItem();
         }
     };
 
     reader.readAsBinaryString(f);
 }
 
+function updateNewItem() {
+    setTimeout(function () {
+        var ele = document.querySelector('.new-plate');
+        if (!ele) return;
+
+        ele.classList.remove('new-plate');
+
+        var name = ele.parentElement.getAttribute('data-pair-name');
+        var data = fileData[name];
+
+        document.getElementById(encodeURI(name)).classList.add('show');
+
+        switch (ele.getAttribute('data-plate-type')) {
+            case 'image':
+                if (data.hasOwnProperty('image')) {
+                    handleImage(data['image'], ele);
+                } else {
+                    updateNewItem();
+                }
+                break;
+            case 'heatmap':
+                if (data.hasOwnProperty('data')) {
+                    handleDat(data['data'], ele);
+                } else {
+                    updateNewItem();
+                }
+                break;
+        }
+    }, 0);
+}
+
 function handleFileSelect(evt) {
     var files = evt.target.files; // FileList object
-    var base, names = [], data;
-    var pairsParent, div;
+    var base, names = [], data, i, f, name, tally = {'images': 0, 'dats': 0}, colSize;
+    var pairsParent, div, inner;
 
     pairsParent = document.getElementById('pairs');
     pairsParent.innerHTML = ''; // Clear element
     fileData = {};
 
-    for (var i = 0, f; f = files[i]; i++) {
+    for (i = 0; f = files[i]; i++) {
         base = getBaseName(f.name);
 
         if (!fileData.hasOwnProperty(base)) {
@@ -101,36 +135,42 @@ function handleFileSelect(evt) {
         }
 
         if (f.type.match('image.*')) {
+            tally['images']++;
             fileData[base]['image'] = f;
         } else {
+            tally['dats']++;
             fileData[base]['data'] = f;
         }
     }
 
     names.sort();
 
-    for (var i = 0, name; name = names[i]; i++) {
+    colSize = tally['images'] && tally['dats'] ? '6' : '12';
+
+    for (i = 0; name = names[i]; i++) {
         data = fileData[name];
 
         div = document.createElement('div');
         div.classList.add('row');
         div.classList.add('py-2');
+        div.setAttribute('data-pair-name', name);
 
-        div.innerHTML = [
-            '<div class="col-sm-12"><h4>', name, '</h4></div>',
-            '<div class="col-sm-6" id="', encodeURI(name), '-image"></div>',
-            '<div class="col-sm-6" id="', encodeURI(name), '-heatmap"></div>'].join('');
+        inner = ['<div class="col-sm-12 fade" id="', encodeURI(name), '"><h4>', name, '</h4></div>'];
+
+        if (tally['images']) {
+            inner = inner.concat(['<div class="col-sm-', colSize, ' fade new-plate" data-plate-type="image" id="', encodeURI(name), '-image"></div>']);
+        }
+
+        if (tally['dats']) {
+            inner = inner.concat(['<div class="col-sm-', colSize, ' fade new-plate" data-plate-type="heatmap" id="', encodeURI(name), '-heatmap"></div>'])
+        }
+
+        div.innerHTML = inner.join('');
 
         pairsParent.insertBefore(div, null);
-
-        if (data.hasOwnProperty('image')) {
-            handleImage(data['image'], document.getElementById(encodeURI(name) + '-image'));
-        }
-
-        if (data.hasOwnProperty('data')) {
-            handleDat(data['data'], document.getElementById(encodeURI(name) + '-heatmap'));
-        }
     }
+
+    updateNewItem();
 }
 
 document.getElementById('files').addEventListener('change', handleFileSelect, false);
