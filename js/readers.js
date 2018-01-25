dc7ed1f6ce7d7ae425281570d82b54a5f112c26f
usajusaj <usajusaj@gmail.com>
2018-01-25 16:48:33 -0500

    Support 384 format as well
    Signed-off-by: usajusaj <usajusaj@gmail.com>

--- js/readers.js
+++ js/readers.js
@@ -2,15 +2,19 @@ function readColony(data) {
     var lines = data.match(/[^\r\n]+/g);
 
     var arraySize = lines.length - 13;
-    var sizes;
+    var sizes, rows;
 
     if (arraySize == 1536) {
         sizes = math.zeros(32, 48)._data;
+        rows = 32;
+    } else if (arraySize == 384) {
+        sizes = math.zeros(16, 24)._data;
+        rows = 16;
     }
 
     for (var i = 13, l; l = lines[i]; i++) {
         l = l.match(/[^\s]+/g);
-        sizes[32 - parseInt(l[0])][parseInt(l[1]) - 1] = parseInt(l[2]);
+        sizes[rows - parseInt(l[0])][parseInt(l[1]) - 1] = parseInt(l[2]);
     }
 
     return sizes;
@@ -20,15 +24,19 @@ function readGitter(data) {
     var lines = data.match(/[^\r\n]+/g);
 
     var arraySize = lines.length - 4;
-    var sizes;
+    var sizes, rows;
 
     if (arraySize == 1536) {
         sizes = math.zeros(32, 48)._data;
+        rows = 32;
+    } else if (arraySize == 384) {
+        sizes = math.zeros(16, 24)._data;
+        rows = 16;
     }
 
     for (var i = 4, l; l = lines[i]; i++) {
         l = l.match(/[^\s]+/g);
-        sizes[32 - parseInt(l[0])][parseInt(l[1]) - 1] = parseInt(l[2]);
+        sizes[rows - parseInt(l[0])][parseInt(l[1]) - 1] = parseInt(l[2]);
     }
 
     return sizes;
